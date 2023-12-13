16bc65072938d2f12519b70c8665500f5d17db07
yahya <yahya.mohamed@smartdev.ai>
2023-12-13 14:01:16 +0000

    test api

---
+++ lib/test_api/list_of_hotels_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListOfHotels extends StatelessWidget {
+  const ListOfHotels({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        
+      ),
+    );
+  }
+}
